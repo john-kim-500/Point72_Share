b6a00d649dbc4a31cd8767498ef55865b732edef
john-kim-500 <39680573+john-kim-500@users.noreply.github.com>
2019-02-18 15:08:21 -0500

    Oil Output modelling
    Initial submission (Well model only)

---
+++ Point72/Model/Well.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point72
+{
+    /// <summary>
+    /// Models of the well output
+    /// </summary>
+    public class Well
+    {
+        private double initialOutput;
+        private double dailyDecayAmount;
+
+        /// <summary>
+        /// Create instance of <see cref="Well"/>
+        /// </summary>
+        /// <param name="initialOutput">Initial well output (in number of barrels)</param>
+        /// <param name="dailyDecayAmount">The reduction of output per day (in number of barrels)</param>
+        public Well(double initialOutput, double dailyDecayAmount)
+        {
+            this.initialOutput = initialOutput;
+            this.dailyDecayAmount = dailyDecayAmount;
+        }
+
+        public double DailyOutput(int elapsedDay)
+        {
+            double ret = (this.initialOutput - this.dailyDecayAmount * elapsedDay);
+            if (ret<0D)
+            {
+                ret = 0D;
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ Point72/Test/Well.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Point72.Model;
+
+namespace Point72.Model.Test
+{
+    /// <summary>
+    /// Test cases for the Well class model
+    /// </summary>
+    [TestFixture]
+    public static class WellTest
+    {
+        /// <summary>
+        /// Test initial well output
+        /// </summary>
+        [TestCase]
+        public static void InitialOutput()
+        {
+            Well testWell = new Well(10, 0.5);
+
+            Assert.AreEqual(10, testWell.DailyOutput(0));
+
+            testWell = new Well(5, 100);
+
+            Assert.AreEqual(5, testWell.DailyOutput(0));
+        }
+
+        /// <summary>
+        /// Test various decay rates
+        /// </summary>
+        [TestCase]
+        public static void DecayRate()
+        {
+            Well testWell = new Well(12, 0.5);
+            Assert.AreEqual(6, testWell.DailyOutput(12));
+            Assert.AreEqual(0, testWell.DailyOutput(24));
+
+            testWell = new Well(100, 10);
+            Assert.AreEqual(50, testWell.DailyOutput(5));
+            Assert.AreEqual(0, testWell.DailyOutput(10));
+        }
+
+        /// <summary>
+        /// Ensure output is never negative
+        /// </summary>
+        [TestCase]
+        public static void ZeroOutputAfterExceedingLife()
+        {
+            Well testWell = new Well(12, 0.5);
+            Assert.AreEqual(0, testWell.DailyOutput(1000));
+
+            testWell = new Well(100, 10);
+            Assert.AreEqual(0, testWell.DailyOutput(1000));
+        }
+    }
+}
